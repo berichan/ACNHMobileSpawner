c8765ea64feed3dd92fcc6ada16967fbdf56f584
berichan <berichandev@gmail.com>
2020-07-08 16:50:24 +0100

    A few standalone-specific functionality changes

---
+++ ACNHMS_Source/Assets/Scripts/Tools/Gfx/GraphicSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicSetter : MonoBehaviour
+{
+    private float lastWidth;
+    private float lastHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+#if UNITY_STANDALONE
+        Screen.SetResolution((int)(Screen.height * (9f / 16f)), Screen.height, false, 60);
+        Screen.SetResolution(Screen.width, (int)(Screen.width * (16f / 9f)), false, 60);
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
