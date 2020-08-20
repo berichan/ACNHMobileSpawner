b77edc614dca0aa1ed975465977f484617ae3d6f
berichan <berichandev@gmail.com>
2020-08-20 21:19:13 +0100

    Add screen percent changer

--- ACNHMS_Source/Assets/Scripts/Tools/Gfx/GraphicSetter.cs
+++ ACNHMS_Source/Assets/Scripts/Tools/Gfx/GraphicSetter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GraphicSetter : MonoBehaviour
@@ -7,12 +8,28 @@ public class GraphicSetter : MonoBehaviour
     private float lastWidth;
     private float lastHeight;
 
+    private string configPath = Path.Combine(Directory.GetCurrentDirectory(), "config.txt");
+
     // Start is called before the first frame update
     void Start()
     {
         Application.runInBackground = true;
 #if UNITY_STANDALONE
-        int height = (int) (Screen.currentResolution.height * 0.75f);
+        var screenPercent = 0.75f;
+        if (File.Exists(configPath))
+        {
+            try
+            {
+                var pc = File.ReadAllText(configPath);
+                var nSize = float.Parse(pc);
+                if (nSize > 1)
+                    nSize *= 0.01f;
+                screenPercent = nSize;
+            }
+            catch { }
+        }
+
+        int height = (int) (Screen.currentResolution.height * screenPercent);
         int width = (int)(height * (9f / 16f));
         Screen.SetResolution(width, height, false, 60);
 #endif
