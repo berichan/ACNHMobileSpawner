14f4e5aa42aabc5278cddaae2976d37ef3b96e11
berichan <berichandev@gmail.com>
2021-01-05 02:29:56 +0000

    Add field item block for item and extensions

---
+++ ACNHMS_Source/Assets/Scripts/ACNHMS/Helpers/NHSE/FieldItemBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NHSE.Core;
+
+public class FieldItemBlock
+{
+    private readonly FieldItemLayer Layer;
+    private readonly int X, Y;
+
+    public Item SelectedItem { get => Layer.GetTile(X, Y); }
+
+    public FieldItemBlock(FieldItemLayer layer, int x, int y)
+    {
+        Layer = layer;
+        X = x; Y = y;
+    }
+
+    public void UpdateItem(Item refItem)
+    {
+        if (!refItem.IsNone)
+        {
+            SelectedItem.CopyFrom(refItem);
+            Layer.SetExtensionTiles(SelectedItem, X, Y);
+        }
+        else
+        {
+            SelectedItem.Delete();
+            Layer.DeleteExtensionTiles(SelectedItem, X, Y);
+        }
+    }
+}
