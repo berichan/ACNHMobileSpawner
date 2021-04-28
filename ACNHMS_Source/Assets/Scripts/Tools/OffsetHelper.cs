0ab3b6c1f28c42b606b88239b0049ac67c07d422
berichan <66521620+berichan@users.noreply.github.com>
2021-04-28 20:14:22 +0100

    Update offsets for 1.10.0

--- ACNHMS_Source/Assets/Scripts/Tools/OffsetHelper.cs
+++ ACNHMS_Source/Assets/Scripts/Tools/OffsetHelper.cs
@@ -10,7 +10,7 @@ public static class OffsetHelper
     public const ulong PlayerOtherStartPadding = 0x36A50;
 
     // player other 
-    public const ulong InventoryOffset = 0xAE19C778; // player 0 (A) 
+    public const ulong InventoryOffset = 0xAE61F840; // player 0 (A) 
     private const ulong playerOtherStart = InventoryOffset - 0x10; // helps to get other values, unused 
 
     public const ulong WalletAddress = InventoryOffset + 0xB8;
@@ -25,37 +25,36 @@ public static class OffsetHelper
     public static ulong getTownNameAddress(ulong invOffset) => getPlayerIdAddress(invOffset) - 0xB8 + 0x04;
 
     // main save offsets
-    public const ulong TurnipAddress = 0xAD195B74;
-    public const ulong VillagerAddress = TurnipAddress - 0x2cb0 - 0x43be1c + 0x10 - 0x90;
-    public const ulong VillagerHouseAddress = TurnipAddress - 0x2cb0 - 0x43be1c + 0x43abd4 - 0x90;
-    public const ulong BackupSaveDiff = 0x86D580;
+    public const ulong TurnipAddress = 0xAD618B84;
+    public const ulong VillagerAddress = TurnipAddress - 0x2d40 - 0x43be2c + 0x10;
+    public const ulong VillagerHouseAddress = TurnipAddress - 0x2d40 - 0x43be2c + 0x43abd4;
+    public const ulong BackupSaveDiff = 0x86D590;
 
-    public const ulong FieldItemStart = VillagerAddress - 0x10 + 0x22e1a8;
+    public const ulong FieldItemStart = VillagerAddress - 0x10 + 0x22e1b8;
     public const ulong LandMakingMapStart = FieldItemStart + 0xAAA00;
     public const ulong OutsideFieldStart = FieldItemStart + 0xCF998;
     public const ulong MainFieldStructurStart = FieldItemStart + 0xCF600;
 
     // other addresses
-    public const ulong ArriverNameLocAddress = 0xB6351EA0;
+    public const ulong ArriverNameLocAddress = 0xB66D6FF8;
     public const ulong ArriverVillageLocAddress = ArriverNameLocAddress - 0x1C;
 
-    public const ulong TextSpeedAddress = 0xBA88BC8; 
+    public const ulong TextSpeedAddress = 0xBACE3B8; 
     public const ulong ChatBufferSize = 0x1E;
 
-    public const ulong DodoAddress = 0xA98D15C;
-    public const ulong OnlineSessionAddress = 0x920C740;
-    public const ulong OnlineSessionVisitorAddress = 0x9D3BFB0;
+    public const ulong DodoAddress = 0xA98F15C;
+    public const ulong OnlineSessionAddress = 0x920E740;
+    public const ulong OnlineSessionVisitorAddress = 0x9F974B8;
     public const ulong OnlineSessionVisitorSize = 0x448; // reverse order
 
-    public const ulong TimeAddress = 0x0BA7FCF8;
+    public const ulong TimeAddress = 0x0BAC54E8;
 
     // pointers
-    public static readonly long[] PlayerCoordJumps = new long[5] { 0x39DC030L, 0x18L, 0x178L, 0xD0L, 0xD8L };
-    public static readonly long[] ChatCoordJumps = new long[2] { 0x03C3EED0L, 0x40L };
+    public static readonly long[] PlayerCoordJumps = new long[5] { 0x3A08B40L, 0x18L, 0x178L, 0xD0L, 0xD8L };
+    public static readonly long[] ChatCoordJumps = new long[2] { 0x03CCD158L, 0x40L };
 
-    // exefs (main). Thanks to Arch9sk7 for these
-    public const ulong AnimationSpeedOffset = 0x037A41E8;
-    public const ulong WalkSpeedOffset = 0x01115CE0;
-    public const ulong CollisionOffset = 0x01084140;
-    public const ulong TimeStateAddress = 0x002704C0;
+    // exefs (main)
+    public const ulong AnimationSpeedOffset = 0x037D0090;
+    public const ulong WalkSpeedOffset = 0x01134780;
+    public const ulong TimeStateAddress = 0x00278C20;
 }
